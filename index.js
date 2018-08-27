a3cf52d3d9663712d363d181111e5b8148700c71
Adam Xiao <xiaoadamxiao@gmail.com>
2018-08-27 01:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,16 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  switch(string){
-    case "hello":
-      return "I can\'t hear you!"
-      break;
-    case "HELLO":
-      return "YES INDEED!"
-      break;
-    case "I love you, Grandma.":
-      return "I love you, too."
-      break;
-    }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+   if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+   if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
